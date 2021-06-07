34f5bbc1e36393b7443e09ef57e37d0a9e441e79
Noncenze <thmas.dee@gmail.com>
2021-06-07 10:11:32 -0400

    Models: add schema to User.js

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+// =========================================================
 // Imports
+// =========================================================
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -6,29 +8,48 @@ const cors = require('cors');
 const passport = require('passport');
 const PORT = process.env.PORT || 8000;
 
+
+// =========================================================
 // API
+// =========================================================
 const users = require('./api/users');
 
+
+// =========================================================
 // Middleware
+// =========================================================
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+// =========================================================
 // Initialize Passport and use config file
+// =========================================================
 app.use(passport.initialize());
 
-// Home route
+
+// =========================================================
+// HOME ROUTE
+// =========================================================
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'Smile, you are being watched by the Backend Engineering Team' });
 });
 
+
+// =========================================================
 // Routes
+// =========================================================
 app.use('/api/users', users);
 
 app.get('/*', (req, res) => {
     res.status(404).json({ message: 'Data not found' });
 });
 
+
+// =========================================================
+// Listening / Server
+// =========================================================
 app.listen(PORT, () => {
     console.log(`Server is listening 🎧 on port: ${PORT}`);
 });
